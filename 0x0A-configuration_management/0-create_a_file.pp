c2036cdc3d0d161eebc0756aef3bebdb970facf6
vicano-code <vc.anokwuru@gmail.com>
2024-04-23 09:21:59 -0700

    added path atttribute $ puppet-lint style correction

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,5 +1,8 @@
+# Using puppet, create a file with below specifications
+
 file { '/tmp/school':
   ensure  => file,
+  path    => '/tmp/school',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
