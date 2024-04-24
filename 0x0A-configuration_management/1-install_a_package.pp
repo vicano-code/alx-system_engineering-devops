cb7e907d652e2bcc3cd33887512b0b92a6c53f41
vicano-code <vc.anokwuru@gmail.com>
2024-04-24 10:24:10 -0700

    puppet file for installing package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,19 @@
-package { 'flask':
-  ensure   => 'latest',
-  provider => 'pip3',
+# Using Puppet, install flask from pip3.
+# Requirements:
+# Install flask
+# Version must be 2.1.0
+
+
+# First install puppetlabs-stdlib: > puppet module install puppetlabs-stdlib
+
+# Ensure python3-pip is installed
+package { 'python3-pip':
+  ensure => installed,
 }
 
+# Install Flask version 2.1.0 using pip3
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  path    => '/usr/local/bin:/usr/bin:/bin',
+  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
+}
