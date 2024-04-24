34377880b3682cf266fb1891c1d99dc550754e88
vicano-code <vc.anokwuru@gmail.com>
2024-04-24 12:16:30 -0700

    puppet-lint style correction

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -11,7 +11,7 @@ package { 'python3-pip':
 
 # Install Flask version 2.1.0 using pip3
 exec { 'install_flask':
-  command  => 'pip3 install flask==2.1.0',
-  path     => '/usr/bin/',
+  command => 'pip3 install flask==2.1.0',
+  path    => '/usr/bin/',
   unless  =>  'pip3 list | grep flask',
 }
