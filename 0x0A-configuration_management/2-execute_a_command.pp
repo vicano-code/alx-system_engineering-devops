a2dba0c6019584d1257b25a9a5c09603b33d3572
vicano-code <vc.anokwuru@gmail.com>
2024-04-24 11:01:41 -0700

    file review and update

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,6 @@
-exec { 'kill_killmenow_process':
-  command     => 'pkill killmenow',
-  refreshonly => true,
+# A Puppet manifest that kills a process named killmenow.
+
+exec { 'pkill_killmenow_process':
+  command  => '/usr/bin/pkill -f /killmenow',
 }
 
