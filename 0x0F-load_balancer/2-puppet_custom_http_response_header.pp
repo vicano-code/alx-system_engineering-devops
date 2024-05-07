782d862e7c81b3c6db847b7642b51d1aa5cb510f
vicano-code <vc.anokwuru@gmail.com>
2024-05-07 08:08:20 -0700

    code correction

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -2,28 +2,29 @@
 # and add a custom HTTP header
 
 exec {'update system':
-	command => '/usr/bin/apt-get update'
+	command  => 'sudo apt-get update',
+	provider => shell,
 }
 
 package {'nginx':
-	ensure => 'installed'
+	ensure  => installed,
+	require => Exec['update'],
 }
 
-file {'/var/www/html/index.html':
-	content => 'Hello World!'
+file_line {'headercustom':
+	ensure  => present,
+	path    => '/etc/nginx/sites-available/default',
+	after   => ':80 default_server;',
+	line    => "add_header X-Served-By ${hostname};",
+	require => Package['nginx'],
 }
 
 exec {'redirect_me':
 	command => 'sed -i "48i "\\\tlocation /redirect_me {\n\t\treturn 301 https://google.com;\n\t}\n" /etc/nginx/sites-available/default,
-	provider => 'shell'
-}
-
-nginx::resource::location { 'add_custom_header':
-	location => '/',
-	header   => 'X-Served-By $hostname',
+	provider => 'shell',
 }
 
 service {'nginx':
 	ensure => running,
-	require => Package['nginx']
+	require => File_line['headercustom'],
 }
