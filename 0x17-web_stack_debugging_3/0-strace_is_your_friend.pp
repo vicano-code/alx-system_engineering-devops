ccc4f23ea49998c254d06400689b0051d5a21164
vicano-code <vc.anokwuru@gmail.com>
2024-06-05 09:52:50 -0700

    test code to correct errors

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,13 +1,29 @@
 # Puppet file to automate process to fix Apache2 error code 500
 
-file { '/var/www/html/index.html':
-  ensure  => file,
-  require => File['/var/www/html'],
+file { '/var/www/html':
+  ensure => directory,
+  owner  => 'www-data',
+  group  => 'www-data',
+  mode   => '0755',
 }
 
-# apply fix for wrong file name extension
-exec { 'rename-wrong-file':
-  command => '/bin/mv /var/www/html/wp-includes/class-wp-locale.phpp /var/www/html/wp-includes/class-wp-locale.php',
-  onlyif  => 'test -e /var/www/html/wp-includes/class-wp-locale.phpp',
-  require => File['/var/www/html/wp-includes/class-wp-locale.php'],
+# Create a simple index.html file
+file { '/var/www/html/index.html':
+  ensure  => file,
+  content => '<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to My Website</title>
+</head>
+<body>
+    <h1>Hello, World!</h1>
+    <p>Welcome to my website. This is a simple HTML file served by Apache.</p>
+</body>
+</html>',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0644',
+  require => File['/var/www/html'],
 }
