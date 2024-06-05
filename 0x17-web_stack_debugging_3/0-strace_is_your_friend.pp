50486eaaa22500c9adccfbf48801e7d94cecc8c6
vicano-code <vc.anokwuru@gmail.com>
2024-06-05 09:32:58 -0700

    some edit

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,33 +1,7 @@
 # Puppet file to automate process to fix Apache2 error code 500
-# Error found using strace says no index.html file.
-# Using puppet to create file with content
 
-# Ensure the /var/www/html directory exists with correct ownership and permissions
-file { '/var/www/html':
-  ensure => directory,
-  owner  => 'www-data',
-  group  => 'www-data',
-  mode   => '0755',
-}
-
-# Create a simple index.html file
 file { '/var/www/html/index.html':
   ensure  => file,
-  content => '<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome to My Website</title>
-</head>
-<body>
-    <h1>Hello, World!</h1>
-    <p>Welcome to my website. This is a simple HTML file served by Apache.</p>
-</body>
-</html>',
-  owner   => 'www-data',
-  group   => 'www-data',
-  mode    => '0644',
   require => File['/var/www/html'],
 }
 
@@ -36,11 +10,4 @@ exec { 'rename-wrong-file':
   command => '/bin/mv /var/www/html/wp-includes/class-wp-locale.phpp /var/www/html/wp-includes/class-wp-locale.php',
   onlyif  => 'test -e /var/www/html/wp-includes/class-wp-locale.phpp',
   require => File['/var/www/html/wp-includes/class-wp-locale.php'],
-}
-
-# Ensure the Apache service is running and enabled
-service { 'apache2':
-  ensure    => running,
-  enable    => true,
-  subscribe => File['/var/www/html/index.html'],
 }
