26acd6a1274565358ae3c4433b23bc57326e6e1c
vicano-code <vc.anokwuru@gmail.com>
2024-06-06 10:05:01 +0100

    rewrite heading comment & rearrange imports alphabetically

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 """
-queries the Reddit API and prints the titles of the first
-10 hot posts listed for a given subreddit
-If not a valid subreddit, print None
+queries Reddit API for titles of top 10 hot posts for a given subreddit
 """
-import requests
 import json
+import requests
 
 
 def top_ten(subreddit):
