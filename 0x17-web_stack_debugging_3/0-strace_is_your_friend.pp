17d519d0af03b16147774efb99755281a61a94cb
vicano-code <vc.anokwuru@gmail.com>
2024-06-07 03:33:17 -0700

    syntax correction

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Puppet file to fix typo in wordpress config file causing apache2 error code 500
 
 exec { 'fix typo in wp-settings.ph file':
-  command => "sed i 's/.phpp/.php/' /var/www/html/wp-settings.php",
+  command => "sed -i 's/.phpp/.php/' /var/www/html/wp-settings.php",
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
