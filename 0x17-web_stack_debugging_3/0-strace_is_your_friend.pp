5ba31eb8c659fab5602103a3e690e142998bcee0
vicano-code <vc.anokwuru@gmail.com>
2024-06-07 03:27:49 -0700

    rewrite code to fix typo in config file

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,29 +1,6 @@
-# Puppet file to automate process to fix Apache2 error code 500
+# Puppet file to fix typo in wordpress config file causing apache2 error code 500
 
-file { '/var/www/html':
-  ensure => directory,
-  owner  => 'www-data',
-  group  => 'www-data',
-  mode   => '0755',
-}
-
-# Create a simple index.html file
-file { '/var/www/html/index.html':
-  ensure  => file,
-  content => '<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome to My Website</title>
-</head>
-<body>
-    <h1>Hello, World!</h1>
-    <p>Welcome to my website. This is a simple HTML file served by Apache.</p>
-</body>
-</html>',
-  owner   => 'www-data',
-  group   => 'www-data',
-  mode    => '0644',
-  require => File['/var/www/html'],
+exec { 'fix typo in wp-settings.ph file':
+  command => "sed i 's/.phpp/.php/' /var/www/html/wp-settings.php",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
